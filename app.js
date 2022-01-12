387de129bec6842b77123fdda12dfce9e3ba5fa3
justin woodard <jwoodard2092@gmail.com>
2022-01-12 09:57:48 -0600

    javascript_operators

--- app.js
+++ app.js
@@ -5,15 +5,57 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
+let a='20';
+let b='4';
+let add=('a'+'b');
+let minus=('a'-'b');
+let multiply =('a'*'b');
+let dividing=('a'/'b');
 
+console.log(a);
+console.log(b);
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(dividing);
 // YOUR CODE HERE
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
+let a=11;
+let str= '11';
+let str2='eleven';
+let isPresent=true;
+let firstName = 'Justin';
+let lastName='Woodard';
+
+console.log(a+str); //1111
+console.log(a+str2); //11eleven
+console.log(a+ispresent); //12
+console.log(a+firstName); //11Justin
+console.log(a+lastName); //11Woodard
 // YOUR CODE HERE
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+console.log(d == str3); //true
+console.log(d === str4) //false
+console.log(!isPresent2); //true
+console.log('eleven' == str3); //false
+console.log(d>=str4); //false
+console.log(“eleven” == str3 && d>= str4); //false
+console.log(!isPresent2 /*true*/|| isPresent2 /*false*/); //true
+console.log(0 == false); //true
+console.log(0 === false); //false
+console.log(0 != false); //false
+console.log(0!== false); //true
 
 // YOUR CODE HERE
